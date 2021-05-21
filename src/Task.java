d309de0c3d750be3d385712ab1a13836b29e0da0
jackwang316 <jackwang.jw.jw32@gmail.com>
2021-05-21 14:29:39 -0700

    Added functionality to Task
    The class now has a constructor, custom toString and a method to set the isComplete flag to true.

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,25 @@
+import java.util.GregorianCalendar;
+
 public class Task {
+    String name;
+    String notes;
+    GregorianCalendar dueDate;
+    boolean isComplete;
+
+    public Task(String name, String notes, GregorianCalendar dueDate){
+        this.name = name;
+        this.notes = notes;
+        this.dueDate = dueDate;
+        this.isComplete = false;
+    }
+
+    public void setComplete(){
+        this.isComplete = true;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n" + this.notes + "\n"
+                + this.dueDate + "\n" + this.isComplete + "\n";
+    }
 }
