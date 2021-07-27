4e1e4e9347f0b2825533a6b428dcdd124a5f48f0
jackwang316 <jackwang.jw.jw32@gmail.com>
2021-07-26 18:55:50 -0700

    Added methods to initialize buttons.
    Methods to initialize buttons and their features using
    ActionListeners added.

--- src/ca/cmpt213/a4/view/TaskGUI.java
+++ src/ca/cmpt213/a4/view/TaskGUI.java
@@ -1,4 +1,94 @@
 package ca.cmpt213.a4.view;
 
-public class TaskGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TaskGUI extends JFrame{
+    public final static int WINDOW_WIDTH = 700;
+    public final static int WINDOW_HEIGHT = 720;
+    public final static int BUTTON_PREFER_WIDTH = 100;
+    public final static int BUTTON_PREFER_HEIGHT = 25;
+    JFrame taskFrame;
+    JPanel topButtonPanel;
+    JPanel taskPanel;
+    JPanel bottomButtonPanel;
+    JButton allButton;
+    JButton overdueButton;
+    JButton upcomingButton;
+    JButton addNewButton;
+
+    public TaskGUI() {
+        taskFrame = new JFrame("My To-Do List");
+        taskFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        initAllButton();
+        initOverdueButton();
+        initUpcomingButton();
+
+        initTopPanel();
+        initBottomPanel();
+
+        taskFrame.setVisible(true);
+        taskFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void initAllButton() {
+        allButton = new JButton("All");
+        allButton.setPreferredSize(new Dimension(BUTTON_PREFER_WIDTH, BUTTON_PREFER_HEIGHT));
+        allButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Todo: add stuff related to all button.
+            }
+        });
+    }
+
+    public void initOverdueButton() {
+        overdueButton = new JButton("Overdue");
+        overdueButton.setPreferredSize(new Dimension(BUTTON_PREFER_WIDTH, BUTTON_PREFER_HEIGHT));
+        overdueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Todo: add stuff related to overdue button.
+            }
+        });
+    }
+
+    public void initUpcomingButton() {
+        upcomingButton = new JButton("Upcoming");
+        upcomingButton.setPreferredSize(new Dimension(BUTTON_PREFER_WIDTH, BUTTON_PREFER_HEIGHT));
+        upcomingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Todo: add stuff related to upcoming button.
+            }
+        });
+    }
+
+    public void initTopPanel() {
+        topButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        topButtonPanel.add(allButton);
+        topButtonPanel.add(overdueButton);
+        topButtonPanel.add(upcomingButton);
+        taskFrame.add(topButtonPanel, BorderLayout.NORTH);
+    }
+
+    public void initBottomPanel() {
+        bottomButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        addNewButton = new JButton("Add Task");
+        addNewButton.setPreferredSize(new Dimension(BUTTON_PREFER_WIDTH, BUTTON_PREFER_HEIGHT));
+        bottomButtonPanel.add(addNewButton);
+        taskFrame.add(bottomButtonPanel, BorderLayout.SOUTH);
+    }
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TaskGUI();
+            }
+        });
+    }
 }
